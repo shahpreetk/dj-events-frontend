da91930dcf07c115872abdde4a32bd1f372dde5b
Preet Shah <shahpreetk@gmail.com>
2021-05-23 15:45:48 +0530

    layout connections

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function AboutPage() {
   return (
-    <div>
+    <Layout>
       <h1>About</h1>
       <p>This is a website to find the latest DJ and other musical events</p>
       <p>Version: 1.0.0</p>
-    </div>
+    </Layout>
   );
 }
