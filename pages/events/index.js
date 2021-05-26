58b1c8382e0256c56d6b76a463e105fbcd51d97c
Preet Shah <shahpreetk@gmail.com>
2021-05-26 13:06:58 +0530

    nextjs connected to strapi backend

--- pages/events/index.js
+++ pages/events/index.js
@@ -16,7 +16,7 @@ export default function EventsPage({ events }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events?_sort=date:asc`);
   const events = await res.json();
 
   return {
