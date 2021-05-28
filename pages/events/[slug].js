6dcd5daf2af11cd46ef2bffc70a25a106dbd19af
Preet Shah <shahpreetk@gmail.com>
2021-05-29 00:14:20 +0530

    dashbpard made

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -3,6 +3,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import Link from "next/link";
 import Image from "next/image";
+import EventMap from "@/components/EventMap";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Event.module.css";
@@ -11,36 +12,9 @@ import { useRouter } from "next/router";
 export default function EventPage({ evt }) {
   const router = useRouter();
 
-  const deleteEvent = async (e) => {
-    if (confirm("Are you sure?")) {
-      const res = await fetch(`${API_URL}/events/${evt.id}`, {
-        method: "DELETE",
-      });
-
-      const data = await res.json();
-
-      if (!res.ok) {
-        toast.error(data.message);
-      } else {
-        router.push("/events");
-      }
-    }
-  };
-
   return (
     <Layout>
       <div className={styles.event}>
-        <div className={styles.controls}>
-          <Link href={`/events/edit/${evt.id}`}>
-            <a>
-              <FaPencilAlt /> Edit Event
-            </a>
-          </Link>
-          <a href="#" className={styles.delete} onClick={deleteEvent}>
-            <FaTimes /> Delete Event
-          </a>
-        </div>
-
         <span>
           {new Date(evt.date).toLocaleDateString("en-IN")} at {evt.time}
         </span>
@@ -62,7 +36,7 @@ export default function EventPage({ evt }) {
         <p>{evt.description}</p>
         <h3>Venue: {evt.venue}</h3>
         <p>{evt.address}</p>
-
+        <EventMap evt={evt} />
         <Link href="/events">
           <a className={styles.back}>{"<"} Go Back</a>
         </Link>
